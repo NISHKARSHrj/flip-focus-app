4c6a00a291c9f59e4a23011949387fb94a21205a
NISHKARSHrj <ronindas00@gmail.com>
2026-06-23 18:59:04 +0530

    adding the colors which is use in the project

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF07070D);
+  static const Color card = Color(0xFF11111B);
+  static const Color primary = Color(0xFF8B5CF6);
+  static const Color text = Color(0xFFF8FAFC);
+  static const Color secondaryText = Color(0xFF9CA3AF);
+  static const Color success = Color(0xFF4ADE80);
+  static const Color danger = Color(0xFFDC2626);
+}
